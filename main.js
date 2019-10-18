6f4644cefa9de709428c8aecc3cd128dde68fe98
Caio Gomes <caio.gomes@altec.ws>
2019-10-17 23:09:20 -0300

    promises

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-var xhr = new XMLHttpRequest();
+var minhaPromise = function() {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET','https://api.github.com/users/caioadauri');
+        xhr.send(null);
 
-xhr.open('GET', 'https://api.github.com/users/caioadauri')
-xhr.send(null);
-
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-        console.log(JSON.parse(xhr.responseText));
-    }
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject('Erro na requisição');
+                }
+            }
+        }
+    });
 }
+
+minhaPromise()
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.warn(error);
+    });
